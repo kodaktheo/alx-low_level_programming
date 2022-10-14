ab6d5f56711f68caf0be3f1d7dbc4be220b34a66
Theo Akwemoh <donteeo@gmail.com>
2022-10-14 05:05:40 -0700

    small edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-	char str[] = ("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 
-	puts(str);
 	return (0);
 }
