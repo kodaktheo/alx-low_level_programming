e2eda8b790cac3e620d6c85d7c2302342253d597
Theo Akwemoh <donteeo@gmail.com>
2022-10-14 04:51:04 -0700

    correction of woork

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n";
+	char str[] = ("Programming is like building a multilingual puzzle\n");
+
 	puts(str);
-		return (0);
+	return (0);
 }
